b87bc8baff8f2ea045d0ff89b0460773323a047f
Rheza Prawira Sagita <rheza@floq.id>
2026-04-13 14:09:54 +0700

    Fix SPM integration for version 2.0.42

--- Package.swift
+++ Package.swift
@@ -1,39 +1,46 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Clickstream",
-    platforms: [.macOS(.v10_12),
-                .iOS(.v11),
-                .tvOS(.v10),
-                .watchOS(.v3)],
+    platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Clickstream",
-            targets: ["Clickstream"]),
+        .library(name: "Clickstream", targets: ["Clickstream"]),
+        .library(name: "ClickstreamTracker", targets: ["Clickstream"]),
+        .library(name: "ClickstreamEventVisualizer", targets: ["Clickstream"]),
+        .library(name: "ClickstreamETETestSuite", targets: ["Clickstream"]),
     ],
     dependencies: [
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .exact("1.10.2")),
-        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", from: "5.0.0"),
-        .package(name: "GRDB", url: "https://github.com/groue/GRDB.swift.git", .exact("5.12.0")),
-        .package(name: "Starscream", url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.4")),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.30.0"),
+        .package(url: "https://github.com/ashleymills/Reachability.swift.git", from: "5.0.0"),
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.7.0"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", exact: "4.0.5")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Clickstream",
-            dependencies: ["SwiftProtobuf", "Reachability", "GRDB", "Starscream"],
-            path: "Sources"),
-        .testTarget(
-            name: "ClickstreamTests",
-            dependencies: ["Clickstream"],
-            path: "ClickstreamTests",
-            exclude: ["Info.plist", "Test Plans"],
-            resources: [.process("Resources")])
-    ],
-    swiftLanguageVersions: [.v5]
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "Reachability", package: "Reachability.swift"),
+                .product(name: "GRDB", package: "GRDB.swift"),
+                .product(name: "Starscream", package: "Starscream")
+            ],
+            path: "Sources",
+            resources: [
+                .process("EventVisualizer/Resources"),
+            ],
+            swiftSettings: [
+                .define("TRACKER_ENABLED"),
+                .define("EVENT_VISUALIZER_ENABLED"),
+                .define("ETE_TEST_SUITE_ENABLED"),
+            ],
+            linkerSettings: [
+                .linkedFramework("UIKit"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("CoreTelephony"),
+            ]
+        ),
+    ]
 )
